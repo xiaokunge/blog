7675d0712d0dc3d66c1be5f3f4ea6827ef2057ac
“loyasto” <loyasto@gmail.com>
2020-06-12 15:40:21 +0800

    Site updated: 2020-06-12 15:40:21

--- js/technology.js
+++ js/technology.js
@@ -61,7 +61,7 @@
   // 建议在移动端不初始化，其实 /search.xml 文件还挺大的，
   if ($('.local-search').size()) {
     $.getScript('/blog/js/search.js', function () {
-      searchFunc("/search.xml", 'local-search-input', 'local-search-result');
+      searchFunc("/blog/search.xml", 'local-search-input', 'local-search-result');
     });
   }
 
